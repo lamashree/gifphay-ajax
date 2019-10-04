952fee6e19e256313daaf1cd2d29830b3b38147f
admin <admin@admins-MacBook-Pro.local>
2019-10-03 23:56:43 -0700

    i am almost done for the coding

--- javascript/apps.js
+++ javascript/apps.js
@@ -1,4 +1,4 @@
-  
+
 //seudo code//
 //1. create the array of songs and loop to the every index of song
 //create the function that is assigning clickable button to the every song in the array.
@@ -6,9 +6,9 @@
 // create the another function that preserve the input value from user and add to array of song.
 // get the gifphay key from the gifphay.com
 //make ajax function with url and method
-//get the response and display in the div 
+//get the response and display in the div //
 
-  var songs = ["Old Town Road", "bad guy", "I dont care", "Dancing with A stranger", "You Need To Calm Down", "Boyfriend", "7 rings", "ME!", "Someone You Loved", "juice",];
+var songs = ["Old Town Road", "bad guy", "I dont care", "Dancing with A stranger", "You Need To Calm Down", "Boyfriend", "7 rings", "ME!", "Someone You Loved", "juice",];
 function renderButtons() {
 
     $("#buttons-view").empty();
@@ -20,72 +20,63 @@ function renderButtons() {
         a.addClass("song");
         a.attr("data-name", songs[i]);
         a.text(songs[i]);
-        $("#song-view").append(a);
+        $("#buttons-view").append(a);
     }
 }
-$("#add-song").on("click", function(event) {
+$("#add-song").on("click", function (event) {
     event.preventDefault();
 
     var song = $("#song-input").val().trim();
     songs.push(song);
-
+    //console.log(songs);
     renderButtons();
-  });
-  renderButtons();
+});
+$(document).on("click", ".movie-btn", displayMovieInfo);
 
+renderButtons();
 
-  var topic = $(this).attr("data-name");
-var api = 'https://api.giphy.com/v1/gifs/search?';
-var limit = '&limit=10';
-var key = 'api_key=1bQ9pVg5vAqAtTnBRvSSBLn8iR4U8Yi4&q=';
-var rating = '&rating=G';
-//queryURL variable
-var queryURL = api + key + topic + limit + rating;
-console.log("queryURL", queryURL);
-//ajax jquery Object
-$.ajax({
-    url: queryURL,
-    method: "GET"
-})
-.then(function (response) {
-    console.log("response",response);
+function displayMovieInfo() {
+    var topic = $(this).attr("data-name");
+    var api = 'https://api.giphy.com/v1/gifs/search?';
+    var limit = '&limit=10';
+    var key = 'api_key=1bQ9pVg5vAqAtTnBRvSSBLn8iR4U8Yi4&q=';
+    //var rating = '&rating=G';
+    //queryURL variable
+    var queryURL = api + key + topic + limit; //+ rating;
+    console.log("queryURL", queryURL);
+    //ajax jquery Object
+    $.ajax({
+        url: queryURL,
+        method: "GET"
+    }).then(function (response) {
+        console.log("67676sstytyttstysy")
+        var resData = response.data;
+        //console.log("response", resData);
+        var songDiv = $("<div class ='song'>");
+        var import_datetime_v = resData.import_datetime;
+        var p = $("<p>").text("datetime;", import_datetime_v);
+        songDiv.append(p);
 
-    var songDiv = $("<div class='song'>");
-    var rating = response.rating;
+        var rating_v = resData.rating;
+        var p = $("<p>").text("rated;", rating_v);
+        songDiv.append(p);
 
+        var image = resData.images.fixed_height_still.url;
+        var p = $("<p>").text("images;", image);
+        songDiv.append(p);
 
-    var ratedisplay = $("<p>").text("Rating: " + rating);
+        var title_v = resData.title;
+        var p = $("<p>").text("title;", title_v);
+        songDiv.append(p);
 
-    songDiv.append(ratedisplay);
-    var import_datetime = response.import_datetime;
+        $("#song-view").prepend(songDiv);
 
-    var pTwo = $("<p>").text("Released: " + import_datetime);
+        console.log(import_datetime_v);
+        console.log(rating_v);
+        console.log(image);
+        console.log(title_v);
+});
 
-    // Displaying the release year
-    songDiv.append(pTwo);
-
-    // Storing the plot
-    var source_post_url = response.source_post_url;
-
-    // Creating an element to hold the plot
-    var pDate = $("<p>").text("Plot: " + source_post_url);
-
-    // Appending the plot
-    songDiv.append(pDate);
-
-    // Retrieving the URL for the image
-    var imgURL = response.images;
-
-    // Creating an element to hold the image
-    var pimage = $("<img>").attr("src", imgURL);
-
-    // Appending the image
-    songDiv.append(pimage);
-
-    // Putting the entire movie above the previous movies
-    $("#movies-view").prepend(songDiv);
-  });
-
-
+}
 
 
